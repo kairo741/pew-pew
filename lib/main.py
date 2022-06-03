5f3112b291b7f459595105ed16f6f40d87bc1c29
jgdml <jgdml2801@gmail.com>
2022-06-02 22:43:06 -0300

    atirar inicial

--- lib/main.py
+++ lib/main.py
@@ -4,53 +4,85 @@ from object.GameObject import GameObject
 from object.Size import Size
 
 
-def render():
+def mainLoop():
 
     pygame.display.init()
 
-    flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.FULLSCREEN
+    flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.HWACCEL | pygame.FULLSCREEN
     flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.HWACCEL
 
     get_res = pygame.display.Info()
-    resolution = [int(get_res.current_w*0.75), int(get_res.current_h * 0.75)]
-    screen = pygame.display.set_mode(resolution, flags)
-    root = path.abspath(path.join(
-        path.dirname(__file__), '..', ''))
+    resolution = Size(x=int(get_res.current_w*0.75),
+                      y=int(get_res.current_h * 0.75),)
+    screen = pygame.display.set_mode([resolution.x, resolution.y], flags)
+    root = path.abspath(path.join(path.dirname(__file__), '..', ''))
     pygame.display.set_caption("PewPew")
     clock = pygame.time.Clock()
-    center = [resolution[0] // 2, resolution[1] // 2]
+    center = [resolution.x // 2, resolution.y // 2]
 
-    bg = GameObject(x=0, y=0, size=Size(resolution[0], resolution[1]),)
+    bg = GameObject(x=0, y=0, size=Size(resolution.x, resolution.y))
 
-    obj = GameObject(x=center[0], y=center[1], size=Size(resolution[0]/75, resolution[0]/75),
-                     sprite=pygame.image.load(root+"\\assets\\images\\ship.png").convert_alpha())
+    player = GameObject(x=center[0], y=center[1],
+                        size=Size(resolution.x*0.1, resolution.x*0.1),
+                        sprite=pygame.image.load(
+        root+"\\assets\\images\\ship.png").convert_alpha())
 
-    pygame.key.set_repeat(16)
+    bulletSprite = pygame.image.load(root+"\\assets\\images\\bullet.png").convert_alpha()
+    bulletSprite = pygame.transform.scale(
+        bulletSprite, (10, 50))
+
+    player.sprite = pygame.transform.scale(
+        player.sprite, (player.size.x, player.size.y))
+
+    bullets = []
+
+    lastBullet = 0
 
     while(True):
+        timePassed = clock.tick(60) / 10
+
+        pygame.draw.rect(screen, (50, 50, 50), bg.toRect())
+
+        for bullet in bullets:
+            screen.blit(bulletSprite, bullet.toRect())
+            bullet.y -= 20 * timePassed
+
+
+        screen.blit(player.sprite, player.toRect())
+
+        
+        pygame.display.update()
+
         keys = pygame.key.get_pressed()
 
         if (keys[pygame.K_d]):
-            obj.x += 10
+            player.x += 10 * timePassed
 
         if (keys[pygame.K_a]):
-            obj.x -= 10
+            player.x -= 10 * timePassed
 
         if (keys[pygame.K_w]):
-            obj.y -= 7
+            player.y -= 7 * timePassed
 
         if (keys[pygame.K_s]):
-            obj.y += 7
+            player.y += 7 * timePassed
 
-        if (pygame.event.wait().type == pygame.QUIT):
-            exit()
+        if (keys[pygame.K_SPACE]):
+            if (pygame.time.get_ticks() - lastBullet > 150):
+                
+                bullets.append(GameObject(x=player.x-5+player.size.x/2, y=player.y, size=Size(10, 50), sprite=bulletSprite))
+                lastBullet = pygame.time.get_ticks()
 
-        pygame.draw.rect(screen, (50, 50, 50), bg.toRect())
-        screen.blit(obj.sprite, obj.toRect())
+        for event in pygame.event.get():
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_SPACE):
+                    bullets.append(GameObject(
+                        player.x-5+player.size.x/2, player.y, Size(10, 40)))
 
-        pygame.display.update()
+            if (event.type == pygame.QUIT):
+                pygame.quit()
 
-        clock.tick(60)
+        print(clock.get_fps(), end='\r')
 
 
-render()
+mainLoop()
