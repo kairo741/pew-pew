c9356f31cd6614300e6cecdb57a21baf326f44e5
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-06-07 20:00:14 -0300

    Inicio de função para modo tela cheia

--- lib/main.py
+++ lib/main.py
@@ -10,11 +10,10 @@ from object.Axis import Axis
 
 
 def scaleImage(image, scale):
-    return pygame.transform.smoothscale(image, (image.get_width()*scale, image.get_height()*scale))
+    return pygame.transform.smoothscale(image, (image.get_width() * scale, image.get_height() * scale))
 
 
 def mainLoop():
-
     pygame.display.init()
     pygame.mixer.init()
     pygame.font.init()
@@ -23,44 +22,43 @@ def mainLoop():
     flags = pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.HWACCEL
 
     get_res = pygame.display.Info()
-    resolution = Axis(x=int(get_res.current_w*0.75),
+    resolution = Axis(x=int(get_res.current_w * 0.75),
                       y=int(get_res.current_h * 0.75))
     screen = pygame.display.set_mode([resolution.x, resolution.y], flags)
     root = path.abspath(path.join(path.dirname(__file__), '..', ''))
     pygame.display.set_caption("PewPew")
     clock = pygame.time.Clock()
 
-    bg = GameObject(speed=Axis(0, 8), sprite=pygame.image.load(root+"\\assets\\images\\background.png"))
+    bg = GameObject(speed=Axis(0, 8), sprite=pygame.image.load(root + "\\assets\\images\\background.png"))
     bg.sprite = pygame.transform.smoothscale(bg.sprite, [resolution.x, resolution.y])
     bg.setSizeWithSprite()
 
     bg2 = copy(bg)
     bg2.y = -resolution.y
 
-    
-
-    player = Player(x=resolution.x/2, y=resolution.y/2, speed=Axis(10, 7), sprite=pygame.image.load(root+"\\assets\\images\\ship.png").convert_alpha())
+    player = Player(x=resolution.x / 2, y=resolution.y / 2, speed=Axis(10, 7),
+                    sprite=pygame.image.load(root + "\\assets\\images\\ship.png").convert_alpha())
     player.sprite = scaleImage(player.sprite, 0.5)
     player.setSizeWithSprite()
 
-    bulletSprite = pygame.image.load(root+"\\assets\\images\\bullet.png").convert_alpha()
+    bulletSprite = pygame.image.load(root + "\\assets\\images\\bullet.png").convert_alpha()
     bulletSprite = scaleImage(bulletSprite, 0.2)
 
     bullets = []
     lastBullet = 0
-    
+
     lastEnemy = 0
 
-    enemySprite = pygame.image.load(root+"\\assets\\images\\shipEnemy.png").convert_alpha()
+    enemySprite = pygame.image.load(root + "\\assets\\images\\shipEnemy.png").convert_alpha()
     enemySprite = scaleImage(enemySprite, 0.5)
     enemies = []
     # enemies.append(enemy)
     fps = FPS()
-    
-    explosionSfx = pygame.mixer.Sound(root+"\\assets\\sfx\\Explosion.mp3")
+
+    explosionSfx = pygame.mixer.Sound(root + "\\assets\\sfx\\Explosion.mp3")
     explosionSfx.set_volume(0.2)
 
-    while(True):
+    while (True):
         timePassed = clock.tick(60) / 10
 
         bg.render(screen)
@@ -73,29 +71,29 @@ def mainLoop():
             bg2.y = -resolution.y
             bg.y = 0
 
-        fps.render(display=screen, fps=clock.get_fps(), position=(resolution.x-30, 0))
+        fps.render(display=screen, fps=clock.get_fps(), position=(resolution.x - 30, 0))
 
         for bullet in bullets:
             bullet.render(screen)
             bullet.y += bullet.speed.y * timePassed
-            
+
             if bullet.y < -bullet.size.y:
                 bullets.remove(bullet)
-                    
+
             for e in enemies:
                 if (bullet.toRect().colliderect(e.toRect())):
                     pygame.mixer.Sound.play(explosionSfx)
                     enemies.remove(e)
                     try:
                         bullets.remove(bullet)
-                    except :
+                    except:
                         print("err remove enemy")
-                    
 
         player.render(screen)
 
         if (pygame.time.get_ticks() - lastEnemy > 800):
-            newEnemy = GameObject(x=resolution.x/2, y=0, sprite=pygame.Surface.copy(enemySprite), speed=Axis(randint(-8, 8), randint(0, 4)))
+            newEnemy = GameObject(x=resolution.x / 2, y=0, sprite=pygame.Surface.copy(enemySprite),
+                                  speed=Axis(randint(-8, 8), randint(0, 4)))
             newEnemy.setSizeWithSprite()
             newEnemy.center()
             enemies.append(newEnemy)
@@ -111,7 +109,7 @@ def mainLoop():
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_d]:
-            if (player.x+player.size.x < resolution.x-1):
+            if (player.x + player.size.x < resolution.x - 1):
                 player.x += player.speed.x * timePassed
 
         if keys[pygame.K_a]:
@@ -123,22 +121,21 @@ def mainLoop():
                 player.y -= player.speed.y * timePassed
 
         if (keys[pygame.K_s]):
-            if (player.y+player.size.y < resolution.y-1):
+            if (player.y + player.size.y < resolution.y - 1):
                 player.y += player.speed.y * timePassed
 
         if (keys[pygame.K_SPACE]):
             if (pygame.time.get_ticks() - lastBullet > 200):
-                
                 newBullet = GameObject(speed=Axis(0, -20), sprite=pygame.Surface.copy(bulletSprite))
                 newBullet.setSizeWithSprite()
-                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x+12
+                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x + 12
                 newBullet.y = player.y
                 bullets.append(newBullet)
 
                 newBullet = None
                 newBullet = GameObject(speed=Axis(0, -20), sprite=pygame.Surface.copy(bulletSprite))
                 newBullet.setSizeWithSprite()
-                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x-12
+                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x - 12
                 newBullet.y = player.y
                 bullets.append(newBullet)
                 lastBullet = pygame.time.get_ticks()
@@ -146,6 +143,10 @@ def mainLoop():
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    # TODO - modo tela cheia
+                    DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 
 
 mainLoop()
--- lib/object/Player.py
+++ lib/object/Player.py
@@ -1,9 +1,10 @@
 from .GameObject import GameObject
 from .Axis import Axis
 
+
 class Player(GameObject):
     def __init__(self, x=0, y=0, size=Axis.zero(), speed=Axis.zero(), sprite="", weapon="", health=100):
         super().__init__(x, y, size, speed, sprite)
-        
+
         self.weapon = weapon
         self.health = health
