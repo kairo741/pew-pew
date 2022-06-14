2c3d37cfabc55e67b0845733491cb30ae42af1a8
Cristian Ceccon <Cristian_ceccon45@hotmail.com>
2022-06-14 20:18:25 -0300

    Estrelas geradas aleatorias

--- lib/main.py
+++ lib/main.py
@@ -32,25 +32,27 @@ def mainLoop():
     pygame.display.set_caption("PewPew")
     clock = pygame.time.Clock()
 
-    bg = Background(speed=Axis(0, 8), sprite=pygame.image.load(root + "\\assets\\images\\background.png"))
-    bg.sprite = pygame.transform.smoothscale(bg.sprite, [resolution.x, resolution.y])
-    bg.setSizeWithSprite()
+    bg = Background()
+    bg.star_sprite = scaleImage(pygame.image.load(
+        root + "\\assets\\images\\star.png"), 0.2)
 
     player = Player(x=resolution.x / 2, y=resolution.y / 2, speed=Axis(10, 7),
                     sprite=pygame.image.load(root + "\\assets\\images\\ship.png").convert_alpha())
     player.sprite = scaleImage(player.sprite, 0.5)
     player.setSizeWithSprite()
 
-    bullet_sprite = pygame.image.load(root + "\\assets\\images\\bullet.png").convert_alpha()
-    bullet_sprite = scaleImage(bullet_sprite, 0.2)
+    bulletSprite = pygame.image.load(
+        root + "\\assets\\images\\bullet.png").convert_alpha()
+    bulletSprite = scaleImage(bulletSprite, 0.2)
     bullet_controller = BulletController()
 
-    last_bullet = 0
+    lastBullet = 0
 
-    last_enemy = 0
+    lastEnemy = 0
 
-    enemy_sprite = pygame.image.load(root + "\\assets\\images\\shipEnemy.png").convert_alpha()
-    enemy_sprite = scaleImage(enemy_sprite, 0.5)
+    enemySprite = pygame.image.load(
+        root + "\\assets\\images\\shipEnemy.png").convert_alpha()
+    enemySprite = scaleImage(enemySprite, 0.5)
     enemies = []
 
     fps = FPS()
@@ -58,12 +60,13 @@ def mainLoop():
     explosion_sfx = pygame.mixer.Sound(root + "\\assets\\sfx\\Explosion.mp3")
     explosion_sfx.set_volume(0.2)
 
-    while True:
-        time_passed = clock.tick(60) / 10
+    while (True):
+        timePassed = clock.tick(60) / 10
 
-        bg.render(screen)
+        bg.render_background(screen)
 
-        fps.render(display=screen, fps=clock.get_fps(), position=(resolution.x - 30, 0))
+        fps.render(display=screen, fps=clock.get_fps(),
+                   position=(resolution.x - 30, 0))
         bullet_controller.render_bullets(screen)
 
         for e in enemies:
@@ -71,62 +74,65 @@ def mainLoop():
 
         player.render(screen)
 
-        if pygame.time.get_ticks() - last_enemy > 800:
-            new_enemy = GameObject(x=resolution.x / 2, y=0, sprite=pygame.Surface.copy(enemy_sprite),
+        if (pygame.time.get_ticks() - lastEnemy > 800):
+            newEnemy = GameObject(x=resolution.x / 2, y=0, sprite=pygame.Surface.copy(enemySprite),
                                   speed=Axis(randint(-8, 8), randint(0, 4)))
-            new_enemy.setSizeWithSprite()
-            new_enemy.center()
-            enemies.append(new_enemy)
-            last_enemy = pygame.time.get_ticks()
+            newEnemy.setSizeWithSprite()
+            newEnemy.center()
+            enemies.append(newEnemy)
+            lastEnemy = pygame.time.get_ticks()
 
         for e in enemies:
             e.x += e.speed.x
             e.y += e.speed.y
-            e.render(screen)
+            e.render((screen))
 
         pygame.display.update()
 
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_d]:
-            if player.x + player.size.x < resolution.x - 1:
-                player.x += player.speed.x * time_passed
+            if (player.x + player.size.x < resolution.x - 1):
+                player.x += player.speed.x * timePassed
 
         if keys[pygame.K_a]:
             if player.x > 2:
-                player.x -= player.speed.x * time_passed
+                player.x -= player.speed.x * timePassed
 
-        if keys[pygame.K_w]:
-            if player.y > 2:
-                player.y -= player.speed.y * time_passed
+        if (keys[pygame.K_w]):
+            if (player.y > 2):
+                player.y -= player.speed.y * timePassed
 
-        if keys[pygame.K_s]:
-            if player.y + player.size.y < resolution.y - 1:
-                player.y += player.speed.y * time_passed
+        if (keys[pygame.K_s]):
+            if (player.y + player.size.y < resolution.y - 1):
+                player.y += player.speed.y * timePassed
 
-        if keys[pygame.K_SPACE]:
-            if pygame.time.get_ticks() - last_bullet > 200:
-                new_bullet = GameObject(speed=Axis(0, -20), sprite=pygame.Surface.copy(bullet_sprite))
-                new_bullet.setSizeWithSprite()
-                new_bullet.x = player.getMiddle().x - new_bullet.getMiddle().x + 12
-                new_bullet.y = player.y
-                bullet_controller.shoot(new_bullet)
+        if (keys[pygame.K_SPACE]):
+            if (pygame.time.get_ticks() - lastBullet > 200):
+                newBullet = GameObject(speed=Axis(
+                    0, -20), sprite=pygame.Surface.copy(bulletSprite))
+                newBullet.setSizeWithSprite()
+                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x + 12
+                newBullet.y = player.y
+                bullet_controller.shoot(newBullet)
 
-                new_bullet = None
-                new_bullet = GameObject(speed=Axis(0, -20), sprite=pygame.Surface.copy(bullet_sprite))
-                new_bullet.setSizeWithSprite()
-                new_bullet.x = player.getMiddle().x - new_bullet.getMiddle().x - 12
-                new_bullet.y = player.y
-                bullet_controller.shoot(new_bullet)
-                last_bullet = pygame.time.get_ticks()
+                newBullet = None
+                newBullet = GameObject(speed=Axis(
+                    0, -20), sprite=pygame.Surface.copy(bulletSprite))
+                newBullet.setSizeWithSprite()
+                newBullet.x = player.getMiddle().x - newBullet.getMiddle().x - 12
+                newBullet.y = player.y
+                bullet_controller.shoot(newBullet)
+                lastBullet = pygame.time.get_ticks()
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if (event.type == pygame.QUIT):
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
                     # TODO - modo tela cheia
-                    DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+                    DISPLAYSURF = pygame.display.set_mode(
+                        (0, 0), pygame.RESIZABLE)
 
 
 mainLoop()
--- lib/object/Background.py
+++ lib/object/Background.py
@@ -1,11 +1,41 @@
+from copy import deepcopy
+from random import randrange
 from .GameObject import GameObject
 from .Axis import Axis
+from pygame import time, draw
 
 
 class Background(GameObject):
-    def __init__(self, x=0, y=0, size=Axis.zero(), speed=Axis.zero(), sprite=""):
-        super().__init__(x, y, size, speed, sprite)
+    def __init__(self, x=0, y=0, size=Axis.zero(), speed=Axis.zero(), star_sprite=""):
+        super().__init__(x, y, size, speed)
 
-    # def render(self, screen):
+    stars = []
+    last_render_tick = 0
+    star_render_delay_min = 1000
+    star_render_delay_max = 5000
+    star_spawn_position = Axis(-10, -30)
 
 
+    def make_stars(self, screen_size):
+        get_random_delay = randrange(
+            self.star_render_delay_min, self.star_render_delay_max)
+        last_pos = deepcopy(self.star_spawn_position)
+
+        if time.get_ticks() - self.last_render_tick > get_random_delay:
+            while(last_pos.x < screen_size[0]):
+                last_pos.x = randrange(last_pos.x+10, last_pos.x+90)
+                last_pos.y += randrange(-50, -10)
+
+                self.stars.append(GameObject(x=last_pos.x, y=last_pos.y, speed=Axis(
+                    0, randrange(4, 30)), sprite=self.star_sprite))
+                self.last_render_tick = time.get_ticks()
+
+    def render_background(self, screen):
+        screen_size = screen.get_size()
+        self.make_stars(screen_size)
+        
+        draw.rect(screen, (14, 6, 21), (0, 0, screen_size[0], screen_size[1]), width=0)
+
+        for star in self.stars:
+            star.y += star.speed.y
+            star.render(screen)
