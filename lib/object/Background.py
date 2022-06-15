9f6a15b11fda7fb1fb820dbc7a9d4a47a5d31dd6
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-06-14 22:21:32 -0300

    Fix Background
    Fix no format da file e no self.star_sprite

--- lib/object/Background.py
+++ lib/object/Background.py
@@ -8,6 +8,7 @@ from pygame import time, draw
 class Background(GameObject):
     def __init__(self, x=0, y=0, size=Axis.zero(), speed=Axis.zero(), star_sprite=""):
         super().__init__(x, y, size, speed)
+        self.star_sprite = star_sprite
 
     stars = []
     last_render_tick = 0
@@ -15,15 +16,14 @@ class Background(GameObject):
     star_render_delay_max = 5000
     star_spawn_position = Axis(-10, -30)
 
-
     def make_stars(self, screen_size):
         get_random_delay = randrange(
             self.star_render_delay_min, self.star_render_delay_max)
         last_pos = deepcopy(self.star_spawn_position)
 
         if time.get_ticks() - self.last_render_tick > get_random_delay:
-            while(last_pos.x < screen_size[0]):
-                last_pos.x = randrange(last_pos.x+10, last_pos.x+90)
+            while last_pos.x < screen_size[0]:
+                last_pos.x = randrange(last_pos.x + 10, last_pos.x + 90)
                 last_pos.y += randrange(-50, -10)
 
                 self.stars.append(GameObject(x=last_pos.x, y=last_pos.y, speed=Axis(
@@ -33,7 +33,7 @@ class Background(GameObject):
     def render_background(self, screen):
         screen_size = screen.get_size()
         self.make_stars(screen_size)
-        
+
         draw.rect(screen, (14, 6, 21), (0, 0, screen_size[0], screen_size[1]), width=0)
 
         for star in self.stars:
