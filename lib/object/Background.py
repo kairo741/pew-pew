31c38137fa3beaf102d4af40ec0742733c72bae5
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-06-14 22:34:00 -0300

    Alteração no método de gerar estrelas aleatórias

--- lib/object/Background.py
+++ lib/object/Background.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from random import randrange
+from random import randrange, randint
 from .GameObject import GameObject
 from .Axis import Axis
 from pygame import time, draw
@@ -12,8 +12,8 @@ class Background(GameObject):
 
     stars = []
     last_render_tick = 0
-    star_render_delay_min = 1000
-    star_render_delay_max = 5000
+    star_render_delay_min = 500
+    star_render_delay_max = 600
     star_spawn_position = Axis(-10, -30)
 
     def make_stars(self, screen_size):
@@ -22,13 +22,12 @@ class Background(GameObject):
         last_pos = deepcopy(self.star_spawn_position)
 
         if time.get_ticks() - self.last_render_tick > get_random_delay:
-            while last_pos.x < screen_size[0]:
-                last_pos.x = randrange(last_pos.x + 10, last_pos.x + 90)
-                last_pos.y += randrange(-50, -10)
 
-                self.stars.append(GameObject(x=last_pos.x, y=last_pos.y, speed=Axis(
-                    0, randrange(4, 30)), sprite=self.star_sprite))
-                self.last_render_tick = time.get_ticks()
+            for i in range(0, randrange(5, 15)):
+                self.stars.append(GameObject(x=randrange(0, screen_size[0]), y=randrange(-50, -10),
+                                             speed=Axis(0, randrange(4, 15)), sprite=self.star_sprite))
+
+            self.last_render_tick = time.get_ticks()
 
     def render_background(self, screen):
         screen_size = screen.get_size()
