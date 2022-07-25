f79e43e03a3c6817bde95c7788e760b7fdca6e73
jgdml <jgdml2801@gmail.com>
2022-07-25 19:17:48 -0300

    rotação player speed

--- lib/object/PlayerSpeed.py
+++ lib/object/PlayerSpeed.py
@@ -10,25 +10,35 @@ class PlayerSpeed(Player):
 
         super().__init__(x, y, size, speed, sprite, weapon, health, layout, ultimate)
 
-        self.rotate = False
+        self.rotate = 0
         self.old_sprite = Surface((0, 0))
         self.old_speed = Axis(0, 0)
+
+
 
     def enable_ultimate(self):
         self.old_sprite = self.sprite
         self.old_speed = self.speed
         self.speed = self.speed.scale_to(1.5)
-        self.rotate = True
+        self.rotate = 1
         self.is_invincible = True
         
 
     def disable_ultimate(self):
         self.sprite = self.old_sprite
-        self.rotate = False
+        self.rotate = 0
         self.is_invincible = False
 
     def render(self, screen, render_frame_time):
-        # if self.rotate:
-        #     self.sprite = transform.rotate(self.sprite, 1*render_frame_time).convert_alpha()
+        if self.rotate != 0:
+            self.sprite = transform.rotate(self.old_sprite, self.rotate)
+            self.sprite = transform.scale(self.sprite, self.size.to_list())
+
+            self.rotate-=10
+            print(self.rotate)
+            if self.rotate < -359:
+                self.rotate = 1
+
+            
 
         return super().render(screen, render_frame_time)
