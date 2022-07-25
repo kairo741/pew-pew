3700592e64c320a488f0735d90e65621071f1d9d
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-07-25 01:32:38 -0300

    refac copyobj e espaçamento

--- lib/object/BulletHeal.py
+++ lib/object/BulletHeal.py
@@ -1,27 +1,31 @@
 from copy import deepcopy
+
 from pygame import Surface
+
 from lib.utils.Constants import Constants
 from .Bullet import Bullet
 
+
 class BulletHeal(Bullet):
 
-    def __init__(self, x=0, y=0, size=..., speed=..., sprite="", damage=10, tag=Constants.TAG_PLAYER, source_reference=None):
+    def __init__(self, x=0, y=0, size=..., speed=..., sprite="", damage=10, tag=Constants.TAG_PLAYER,
+                 source_reference=None):
         super().__init__(x, y, size, speed, sprite, damage, tag, source_reference)
 
     def hit_callback(self, object_hit, collision):
         if object_hit.tag == Constants.TAG_PLAYER:
-            heal_amount = self.source_reference.health*0.1
+            heal_amount = self.source_reference.health * 0.1
 
-            if (object_hit.health+heal_amount) < object_hit.max_health:
+            if (object_hit.health + heal_amount) < object_hit.max_health:
                 object_hit.health += heal_amount
             else:
                 object_hit.health = object_hit.max_health
 
     def copy(self):
-        copyobj = BulletHeal()
+        copy_obj = BulletHeal()
         for name, attr in self.__dict__.items():
             if type(attr) is Surface:
-                copyobj.__dict__[name] = Surface.copy(attr)
+                copy_obj.__dict__[name] = Surface.copy(attr)
             else:
-                copyobj.__dict__[name] = deepcopy(attr)
-        return copyobj
+                copy_obj.__dict__[name] = deepcopy(attr)
+        return copy_obj
