cafee761bfbd4c18fc8bd1d47f1543bae00d461e
RafaelBicario <rafabicario@outlook.com>
2022-07-24 22:55:40 -0300

    Speed Disk
    Adição nova sprite do power up Speed, alteração do modelo ingame.

--- lib/utils/Constants.py
+++ lib/utils/Constants.py
@@ -57,7 +57,7 @@ class Constants:
     
     SPRITE_POWER_UP_1 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\atk_disk.png")
     SPRITE_POWER_UP_2 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\pwup-2.png")
-    SPRITE_POWER_UP_3 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\pwup-atk-speed.png")
+    SPRITE_POWER_UP_3 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\speed_disk.png")
     SPRITE_POWER_UP_4 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\pwup-4.png")
     SPRITE_POWER_UP_5 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\pwup-5.png")
     SPRITE_POWER_UP_6 = image.load(ROOT_PATH + "\\assets\\images\\power_ups\\pwup-heal.png")
