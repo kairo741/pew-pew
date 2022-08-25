10835973d90918e275acaff4a7dd6f696d6dbc0b
jgdml <jgdml2801@gmail.com>
2022-08-25 16:58:37 -0300

    berserker outline para skill + ult reseta skill

--- lib/object/players/PlayerBerserk.py
+++ lib/object/players/PlayerBerserk.py
@@ -1,6 +1,6 @@
 from lib.object.game.Ultimate import Ultimate
 from lib.object.players.Player import Player
-from pygame import time
+from pygame import time, draw, mask, Surface, SRCALPHA
 
 class PlayerBerserk(Player):
     def __init__(self, x=0, y=0, size=..., speed=..., sprite="", weapon="", health=100, layout="", ultimate=Ultimate(), level=1):
@@ -8,12 +8,29 @@ class PlayerBerserk(Player):
         ultimate = Ultimate(enable_function=self.enable_ultimate, disable_function=self.disable_ultimate, duration=1, color=[0, 0, 0])
         super().__init__(x, y, size, speed, sprite, weapon, health, layout, ultimate, level)
 
+        self.outline = Surface((0, 0))
+
         # 15 segundos de cooldown
         self.skill_cooldown = 15 * 1000
         self.last_skill = 0
 
         self.death_resistance_skill = True
 
+    def set_size_with_sprite(self, set_glow=True):
+        super().set_size_with_sprite(set_glow)
+
+        self.set_outline()
+
+    def set_outline(self):
+        sprite_mask = mask.from_surface(self.sprite)
+        mask_outline = sprite_mask.outline()
+        self.outline = Surface(self.size.scale_to(2).to_list(), SRCALPHA).convert_alpha()
+        outline_size = self.outline.get_size()
+        
+        for index, point in enumerate(mask_outline):
+            mask_outline[index] = [point[0]+outline_size[0]/4, point[1]+outline_size[1]/4]
+        
+        draw.polygon(self.outline, (255, 50, 50), mask_outline, 3)
 
     def set_level(self, level):
         old_health = self.health
@@ -51,3 +68,12 @@ class PlayerBerserk(Player):
     def enable_ultimate(self):
         super().enable_ultimate()
         self.health = 1
+        self.death_resistance_skill = True
+
+
+    def render(self, screen, render_frame_time):
+        if self.death_resistance_skill:
+            outline_size = self.outline.get_size()
+            screen.blit(self.outline, (self.x-outline_size[0]/4, self.y-outline_size[1]/4))
+
+        return super().render(screen, render_frame_time)
