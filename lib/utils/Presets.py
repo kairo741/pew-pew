9d4a2bc85f854af2efcdaf591edc863af046c5db
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-11-01 19:38:46 -0300

    Vampire Passive & ultimate description

--- lib/utils/Presets.py
+++ lib/utils/Presets.py
@@ -221,8 +221,11 @@ class Presets:
         },
         {
             "name": "Vampire",
-            "passive": "Passive Description",
-            "ultimate": "Ultimate Description",
+            "passive": "Vampire se alimenta da alma de seus inimigos, todo dano causado é convertido em auto-cura, "
+                       "mas a escuridão traz desvantagens, todo orb de cura é purificado e ao ser coletado causa dano"
+                       " a Vampire",
+            "ultimate": "Ao abrir o caixão antigo uma maldição milenar é liberta evocando milhares de morcegos "
+                        "espaciais da dimensão 26, e eles estão sedentos por sangue",
         },
         {
             "name": "Light",
